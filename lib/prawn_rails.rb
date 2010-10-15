61c25b0e81be2bf86f1ee4864d79240c238f253f
Walton Hoops <me@waltonhoops.com>
2010-10-14 18:43:06 -0600

    Attempt to register as a template handler

--- lib/prawn_rails.rb
+++ lib/prawn_rails.rb
@@ -1,1 +1,13 @@
 # PrawnRails
+module Prawn
+  module Rails
+    class TemplateHandler < ActionView::Template::Handler
+      self.default_format= :pdf
+      def render(template)
+        return "Rendering!"
+      end
+    end
+  end
+end
+Mime::Type.register_alias "application/pdf", :pdf
+ActionView::Template.register_template_handler(:prawn, Prawn::Rails::TemplateHandler)
--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,2 @@
 # Include hook code here
+require 'prawn_rails'
