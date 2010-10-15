2c5b36550e20fad8c99734581f5122fcc850a617
Walton Hoops <me@waltonhoops.com>
2010-10-14 21:18:52 -0600

    add PrawnHelper which implements #prawn_document for generating PDFs

--- lib/prawn_rails.rb
+++ lib/prawn_rails.rb
@@ -3,15 +3,21 @@ require 'prawn/layout'
 require 'prawn/security'
 module Prawn
   module Rails
+    module PrawnHelper
+      def prawn_document(opts={})
+        pdf=Prawn::Document.new(opts)
+        yield pdf
+        pdf
+      end
+    end
     class TemplateHandler < ActionView::Template::Handler
       self.default_format= :pdf
       def self.call(template)
-        "pdf=Prawn::Document.new;\n"+
-        "#{template.source}\n"+
-        "pdf.render"
+        "#{template.source}.render"        
       end
     end
   end
 end
 Mime::Type.register_alias "application/pdf", :pdf
 ActionView::Template.register_template_handler(:prawn, Prawn::Rails::TemplateHandler)
+ActionView::Base.send(:include, Prawn::Rails::PrawnHelper)
