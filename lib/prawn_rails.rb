5c045384bc205eb5d57ab8d58d40cd544070682d
Walton Hoops <me@waltonhoops.com>
2010-10-21 20:44:04 -0600

    Update MIT liscense clean up prawn_rails.rb

--- lib/prawn_rails.rb
+++ lib/prawn_rails.rb
@@ -1,23 +1,33 @@
 require 'prawn'
 require 'prawn/layout'
 require 'prawn/security'
+
 module Prawn
   module Rails
+    
     module PrawnHelper
+      
       def prawn_document(opts={})
-        pdf=Prawn::Document.new(opts)
+        pdf = Prawn::Document.new(opts)
         yield pdf
+        
         pdf
       end
+      
     end
+    
     class TemplateHandler < ActionView::Template::Handler
-      self.default_format= :pdf
+      self.default_format = :pdf
+      
       def self.call(template)
         "#{template.source}.render"        
       end
+      
     end
+    
   end
 end
+
 Mime::Type.register_alias "application/pdf", :pdf
 ActionView::Template.register_template_handler(:prawn, Prawn::Rails::TemplateHandler)
 ActionView::Base.send(:include, Prawn::Rails::PrawnHelper)
